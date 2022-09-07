9091dd09ed8ae73601236677f0107b842edb7098
Ilya Zakharov <ilushamdmaa@yandex.ru>
2022-09-07 17:25:10 +0300

    07.09.2022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import 'bootstrap/dist/css/bootstrap.css';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import React, { Component } from 'react';
+import axios from 'axios';
+class App extends Component {
+    state = {
+        todos: []
+    };
+    componentDidMount() {
+        this.getTodos();
+    }
+    getTodos() {
+        axios
+        .get('http://127.0.0.1:8000/api/')
+        .then(res => {
+        this.setState({ todos: res.data });
+        })
+        .catch(err => {
+        console.log(err);
+        });
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="row" className="justify-content-md-center">
+                    {this.state.todos.map(item => (
+                        <div key={item.id}>
+                            <h1>{item.title}</h1>
+                            <span>{item.body}</span>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
-
 export default App;
